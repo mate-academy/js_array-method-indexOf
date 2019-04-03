97808f103432b28031a6fce261f6e8fb949a2c1f
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-03 11:01:33 +0300

    added task description & tests

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,10 +5,10 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
  */
 
-function applyCustomIncludes() {
-  [].__proto__.includes2 = function() {
+function applyCustomIndexOf() {
+  [].__proto__.indexOf2 = function() {
     // write code here
   };
 }
 
-module.exports = applyCustomIncludes;
+module.exports = applyCustomIndexOf;
