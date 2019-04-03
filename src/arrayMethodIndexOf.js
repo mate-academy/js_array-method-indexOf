21918510738d4da4e55ea4a440287bbaeadce16b
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-03 18:13:01 +0300

    updated eslint config. Fixed eslint errors.

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,8 +6,8 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
  */
 
-function applyCustomIndexOf () {
-  [].__proto__.indexOf2 = function () {
+function applyCustomIndexOf() {
+  [].__proto__.indexOf2 = function() {
     // write code here
   };
 }
