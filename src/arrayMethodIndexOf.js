84d1ac6e925296263bd0d8c863a0da9495ae9745
Vitalii <ilovepinkpony1@gmail.com>
2019-04-11 14:09:39 +0300

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,9 +5,18 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function() {
-    // write code here
+  [].__proto__.indexOf2 = function(token, index = 0) {
+    if (index < 0) {
+      index = index + this.length;
+      for (index; index >= 0; index--) {
+        if (this[index] === token) return index;
+      }
+      return -1;
+    }
+    for (index; index < this.length; index++) {
+      if (this[index] === token) return index;
+    }
+    return -1;
   };
 }
-
 module.exports = applyCustomIndexOf;
