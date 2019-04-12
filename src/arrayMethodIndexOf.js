024fd2c72b91ce4996a77c116d0148a8bc10f4d3
ghj ghj <StasGostev>
2019-04-12 17:28:21 +0300

    'Realization_of_IndexOf_method_by_Stas_Gostev'

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,9 +5,25 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function() {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElem, fromIndex = 0) {
+    if (fromIndex >= this.length) {
+      return -1;
+    }
+    for (let i = fromIndex; i < this.length; i++) {
+      // Using isEqualValues cos 'Object.is' looks like a Cheat ;)
+      if (isEqualValues(this[i], searchElem)) {
+        return i;
+      }
+    }
+    return -1;
   };
 }
-
+const isEqualValues = (val1, val2) => {
+  // checking is number for pass verification NaN === NaN
+  if (Number.isNaN(val1) && Number.isNaN(val2)) {
+    return true;
+  } else {
+    return val1 === val2;
+  };
+};
 module.exports = applyCustomIndexOf;
