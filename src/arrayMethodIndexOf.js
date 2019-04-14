a2fa8f4d355f82eb299317e3dde1d258ca21c61c
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-04-14 15:45:45 +0300

    Yevhen Kryvdiuk

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,8 +5,26 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    if (fromIndex >= this.length) {
+      return -1;
+    }
+    let index = 0;
+    fromIndex < 0 ? index = 0 : index = fromIndex;
+    for (index; index < this.length; index++) {
+      if (typeof searchElement === typeof this[index]) {
+        if (searchElement === this[index]) {
+          return index;
+        } else if (isNaN(searchElement) && isNaN(this[index])) {
+          return index;
+        } else {
+          continue;
+        }
+      } else {
+        continue;
+      }
+    }
+    return -1;
   };
 }
 
