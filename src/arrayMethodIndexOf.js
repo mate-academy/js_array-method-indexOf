8712e962a371c5a966c4edbfaf89cda8796aa199
gestap <gestap@gmail.com>
2019-04-15 14:59:50 +0300

    modified solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -7,7 +7,7 @@
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
     return searchElement === undefined ? -1
-      : this.slice(fromIndex).findIndex(el => el === searchElement);
+      : this.slice(fromIndex).findIndex(el => Object.is(el, searchElement));
   };
 }
 
