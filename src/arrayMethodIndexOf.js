6fd9de2bd592e5f4c60ef4fad26eff63077284c0
gestap <gestap@gmail.com>
2019-04-16 14:52:09 +0300

    modified solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,8 +6,19 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    return searchElement === undefined ? -1
-      : this.slice(fromIndex).findIndex(el => Object.is(el, searchElement));
+    // return searchElement === undefined ? -1
+    //   : this.slice(fromIndex).findIndex(el => Object.is(el, searchElement));
+    if (searchElement === undefined) {
+      return -1;
+    } else {
+      let arr = this.slice(fromIndex);
+      for (let i = 0; i < arr.length; i++) {
+        if (Object.is(arr[i], searchElement)) {
+          return i;
+        }
+      }
+      return -1;
+    }
   };
 }
 
