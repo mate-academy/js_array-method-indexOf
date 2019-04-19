f56d540396f558a53e20c74e17e198842d27e648
valeriitovstyk <valeriitovstyk@gmail.com>
2019-04-19 08:52:02 +0300

    changes bunch of if statements into one chain with Or

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -7,10 +7,11 @@
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
     const arrayLength = this.length;
-    if (arrayLength === 0) return -1;
-    if (fromIndex >= this.length) return -1;
-    if (searchElement === undefined) return -1;
-
+    if (arrayLength === 0 ||
+      fromIndex >= this.length ||
+      searchElement === undefined) {
+      return -1;
+    }
     for (let i = fromIndex; i < this.length; i++) {
       if (searchElement === this[i]) return i;
     }
