54980c780ea7e5504a84f9a2795e34d1a6df040e
Onisko <Onisko@MacBook.local>
2019-05-19 12:25:53 +0300

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,7 +6,24 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    if (this.length === 0) {
+      return -1;
+    }
+    let fromIndexInt = fromIndex;
+    if (typeof fromIndex === 'undefined') {
+      fromIndexInt = 0;
+    }
+    let i = fromIndexInt;
+    while (true) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+      i++;
+      if (i >= this.length) {
+        break;
+      }
+    }
+    return -1;
   };
 }
 
