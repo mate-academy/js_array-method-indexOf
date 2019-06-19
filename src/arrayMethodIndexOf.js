a02eff889b10e041b441f939fbdb868aac751bd3
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-06-19 17:12:14 +0300

    fix

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,12 +5,12 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
     if (searchElement === undefined) {
       return -1;
     }
 
-    for (let i = fromIndex || 0; i < this.length; i++) {
+    for (let i = fromIndex; i < this.length; i++) {
       if (this[i] === searchElement) {
         return i;
       }
