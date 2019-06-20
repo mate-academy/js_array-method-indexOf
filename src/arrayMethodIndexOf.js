89231eaa31987a1eabb0bb2c6d194dad4821e136
neitiris <veliantos@gmail.com>
2019-06-20 12:52:31 +0300

    solution commit

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,8 +5,14 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
     // write code here
+    for (let i = fromIndex; i < this.length; i++) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+    }
+    return -1;
   };
 }
 
