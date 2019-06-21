1bd276d36b52e6c7871de0f33403786bc6688015
Kamran <djkamry@gmail.com>
2019-06-22 00:00:32 +0300

    sol

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -4,9 +4,15 @@
  * Implement method indexOf
  *
  */
+
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    for (let i = fromIndex; i < this.length; i++) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+    }
+    return -1;
   };
 }
 
