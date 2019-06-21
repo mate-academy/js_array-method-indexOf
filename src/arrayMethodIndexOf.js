a0e2f45a94b1d746a2c5fbffd8ca77d8f613eb12
tearfulmerlin <t380937709275@gmail.com>
2019-06-21 15:05:38 +0300

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,9 +5,38 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
     // write code here
+    let currentIndex = fromIndex;
+    if (fromIndex < 0) {
+      currentIndex = this.length + fromIndex;
+    };
+
+    for (currentIndex; currentIndex < this.length; currentIndex++) {
+      if (this[currentIndex] === searchElement) {
+        return currentIndex;
+      }
+    }
+
+    return -1;
   };
 }
+
+/* applyCustomIndexOf()
+  *
+  * function showIndexOf() {
+  * const valueToFind = +document
+  *   .querySelector('input[name = "value-to-find"]').value;
+  * const startPosition = +document
+  *   .querySelector('input[name = "start-position"]').value;
+  *
+  * document.getElementsByTagName('p')[0].innerHTML = `
+  *   Postion of element is:
+  *   ${[0, '1', 2, 4, 3, '', undefined, NaN, 6]
+  *   .indexOf2(valueToFind, startPosition)}
+  * `;
+  *
+  * }
+ */
 
 module.exports = applyCustomIndexOf;
