49b8d40ec1872f536b28122e273b489e61ffb4d6
Taisiia <taisiia.pomazan@gmail.com>
2019-06-21 16:24:43 +0300

    sol

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,9 +5,17 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    let i;
+    for (i = fromIndex; i < this.length; i++) {
+      if (this[i] === searchElement) {
+        return i;
+      } else if (searchElement === null) {
+        return -1;
+      };
+    };
+    return -1;
   };
-}
+};
 
 module.exports = applyCustomIndexOf;
