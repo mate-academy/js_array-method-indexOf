b448a2bca3d3c80580b8e95e71ae71fc079f679f
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-06-21 08:23:33 +0300

    Solution
    Solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -1,13 +1,23 @@
 'use strict';
-
 /**
  * Implement method indexOf
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    if (this.length < 1) {
+      return -1;
+    };
+    for (let i = fromIndex; i < this.length; i++) {
+      if (this[i] === searchElement) {
+        return i;
+      };
+      if (searchElement === null) {
+        return -1;
+      };
+      continue;
+    };
+    return -1;
   };
-}
-
+};
 module.exports = applyCustomIndexOf;
