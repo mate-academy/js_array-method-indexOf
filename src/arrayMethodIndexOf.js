789d6936934090fbf618591564209768a5106dac
AndriiSotnikov <andrewsotnikov88@gmail.com>
2019-06-22 14:29:12 +0300

    Solution to custom indexOf

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,8 +5,15 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement = 0, fromIndex = 0) {
+    for (const [index, elem] of this.entries()) {
+      if (index >= fromIndex) {
+        if (elem === searchElement) {
+          return index;
+        }
+      }
+    }
+    return -1;
   };
 }
 
