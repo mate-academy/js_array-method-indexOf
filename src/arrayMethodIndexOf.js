c196eeb8a9cf6e4c5064731019264e3440ec5d58
alex <earthhuman41@gmail.com>
2019-06-22 16:36:28 +0300

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,8 +5,17 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    let i = fromIndex < 0 ? this.length + fromIndex : fromIndex;
+    for (i; i < this.length; i++) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+      if (Number.isNaN(this[i]) && Number.isNaN(searchElement)) {
+        return i;
+      }
+    }
+    return -1;
   };
 }
 
