1e7f56c0ba80575f2659a0bcb860fb9560c9f4de
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-08-21 06:32:18 +0300

    fix negative index

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -4,9 +4,17 @@
  * Implement method indexOf
  *
  */
+const indexNormalisation = (index, length) => {
+  const ind = Math.abs(index) > length - 1
+    ? Math.sign(index) * length
+    : index;
+
+  return ind < 0 ? length + ind : ind;
+};
+
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    let i = fromIndex === undefined ? 0 : fromIndex;
+    let i = fromIndex === undefined ? 0 : indexNormalisation(fromIndex);
 
     while (i < this.length) {
       if (this[i] === searchElement) {
