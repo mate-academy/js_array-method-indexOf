f36f888335afefe5b17653cefbc194b78431ed69
Volodsher <volodsher@gmail.com>
2019-08-25 16:38:06 +0300

    8/10

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,7 +6,20 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let i = 0;
+    if (this.length === 0) {
+      return -1;
+    }
+    if (fromIndex) {
+      i = fromIndex;
+    }
+    for (i; i < this.length - 1; i++) {
+      if (searchElement === this[i]) {
+        return i;
+      } else {
+        return -1;
+      }
+    }
   };
 }
 
