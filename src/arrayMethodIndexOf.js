8f71dc4beb76324ec2d527f7ea32a115262db4a7
Volodsher <volodsher@gmail.com>
2019-08-25 17:38:55 +0300

    1/9

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -4,6 +4,7 @@
  * Implement method indexOf
  *
  */
+
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
     let i = 0;
@@ -13,7 +14,7 @@ function applyCustomIndexOf() {
     if (fromIndex) {
       i = fromIndex;
     }
-    for (i; i < this.length - 1; i++) {
+    for (i; i < this.length; i++) {
       if (searchElement === this[i]) {
         return i;
       } else {
