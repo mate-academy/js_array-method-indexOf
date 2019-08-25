c7ac6328265ce550176be9e2f96acc1c9e1a26d1
Miroslava <777shadowpriest777@gmail.com>
2019-08-25 17:24:04 +0300

    Solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,7 +6,20 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    if (searchElement === undefined || this.length === 0) {
+      return -1;
+    }
+
+    let result = -1;
+
+    for (let i = fromIndex || 0; i < this.length; i++) {
+      if (this[i] === searchElement) {
+        result = i;
+        break;
+      }
+    }
+
+    return result;
   };
 }
 
