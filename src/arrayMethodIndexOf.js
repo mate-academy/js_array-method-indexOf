7d457f87491208dddbcfb38b2bed6edbd7baf217
Vyacheslav Piskun <webdev.pv@gmail.com>
2019-08-27 15:06:52 +0300

    deleted unnecessary comments

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,14 +6,6 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
-    // if (fromIndex < 0)  {
-    //   fromIndex = this.length + fromIndex;
-    // }
-
-    // if (fromIndex < 0) {
-    //   fromIndex = 0;
-    // }
-
     for (let i = fromIndex; i < this.length; i++) {
       if (this[i] === searchElement) {
         return i;
