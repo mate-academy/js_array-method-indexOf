5a3823de23d9a84517eba86e48972747f93ac1b1
Volodsher <volodsher@gmail.com>
2019-08-27 17:11:02 +0300

    refactored

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,23 +6,14 @@
  */
 
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    let i = 0;
-    let checker = 0;
-    if (this.length === 0) {
-      return -1;
-    }
-    if (fromIndex) {
-      i = fromIndex;
-    }
-    for (i; i < this.length; i++) {
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    for (let i = fromIndex; i < this.length; i++) {
       if (searchElement === this[i]) {
         return i;
-      } else {
-        checker = -1;
       }
-    };
-    return checker;
+    }
+
+    return -1;
   };
 }
 
