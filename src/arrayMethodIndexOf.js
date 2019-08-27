dad7d0411162990a48e3581afbe186b630f02f6e
Dmytro <voronok16@gmail.com>
2019-08-27 15:36:03 +0300

    removed unnecessary

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,16 +5,13 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    let firstIndex = fromIndex;
-    if (fromIndex === undefined) {
-      firstIndex = 0;
-    }
-    for (let i = firstIndex; i < this.length; i++) {
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    for (let i = fromIndex; i < this.length; i++) {
       if (this[i] === searchElement) {
         return i;
       }
     }
+
     return -1;
   };
 }
