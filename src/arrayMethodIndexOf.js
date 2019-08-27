3897f880f42c510faa7143a5d82bf73197da78c3
youvovk <wer3wollf@gmail.com>
2019-08-27 15:24:40 +0300

    delete unnecessary rules

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,15 +6,13 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
-    let resultIndexOf = -1;
-
     for (let i = 0; i < this.length; i++) {
       if (i >= fromIndex && this[i] === searchElement) {
-        resultIndexOf = i;
+        return i;
       }
     }
 
-    return resultIndexOf;
+    return -1;
   };
 }
 
