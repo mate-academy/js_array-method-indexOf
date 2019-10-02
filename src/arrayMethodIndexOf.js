ab5006ad3cc8ed7178edcae9c49329d160ca8075
VadimK <djvadya16@gmail.com>
2019-10-02 20:56:45 +0300

    done

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,7 +6,21 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    if (fromIndex) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (searchElement === this[i]) {
+          return i;
+        }
+      }
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        if (searchElement === this[i]) {
+          return i;
+        }
+      }
+    }
+
+    return -1;
   };
 }
 
