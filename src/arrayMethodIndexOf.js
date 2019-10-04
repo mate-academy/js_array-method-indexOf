a2abe983550920bc029f449926e5c21d0325cf3a
ITStan <s.stanislavskaya@gmail.com>
2019-10-04 17:03:46 +0300

    Correction

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,14 +6,12 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
-    const elementNumber = -1;
-
     for (let i = fromIndex; i < this.length; i++) {
       if (searchElement === this[i]) {
         return i;
       }
     }
-    return elementNumber;
+    return -1;
   };
 }
 
