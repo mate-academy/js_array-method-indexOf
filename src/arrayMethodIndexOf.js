246d540cba959f3f5e7c39d46a81a5cf07a2fe9b
nikakarelets <v.karelets@gmail.com>
2019-10-04 17:11:30 +0300

    Fix

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,23 +6,13 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
-    if (searchElement === undefined) {
-      return -1;
-    }
-
     for (let i = fromIndex; i < this.length; i++) {
-      if (typeof searchElement !== typeof this[i]) {
-        return -1;
-      }
-
-      if (this[i].toString() === searchElement) {
+      if (this[i] === searchElement) {
         return i;
       }
-
-      if (this[fromIndex] === searchElement) {
-        return fromIndex;
-      }
     }
+
+    return -1;
   };
 }
 
