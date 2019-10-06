27ff1098a1e285770c4102154613b3545312cc82
Dmitriymush <powder322@gmail.com>
2019-10-06 15:12:05 +0300

    fixed default zero

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,11 +5,8 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    let indFrom = fromIndex;
-    if (indFrom === undefined) {
-      indFrom = 0;
-    }
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    const indFrom = fromIndex;
     for (let i = indFrom; i < this.length; i++) {
       if (searchElement === this[i]) {
         return i;
