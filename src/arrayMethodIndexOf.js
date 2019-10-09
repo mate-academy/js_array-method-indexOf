c24baecc040913d40f6a59ca2f15502a5b9f4939
MMashivskij <mmashivskij@gmail.com>
2019-10-09 13:33:52 +0300

    Solutions

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,9 +5,19 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    let counter = fromIndex;
+    if (this.length === 0) {
+      return -1;
+    };
+    for (counter; counter < this.length; counter++) {
+      if (searchElement === this[counter]) {
+        return counter;
+      }
+    };
+
+    return -1;
   };
-}
+};
 
 module.exports = applyCustomIndexOf;
