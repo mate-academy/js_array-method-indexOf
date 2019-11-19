0b536eaa19172ca64fd8b509927e9420d838fbca
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-11-19 14:41:12 +0200

    fix

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -8,6 +8,13 @@ function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
     let i = fromIndex;
 
+    if (i < 0) {
+      i = this.length + i;
+      if (i < 0) {
+        i = 0;
+      }
+    }
+
     for (i; i < this.length; i++) {
       if (this[i] === searchElement) {
         return i;
