e1a4f976eccd0c6d6394046cada2d5ec70f882a6
Alexandra Boyko <Alexboyko04@gmail.com>
2019-11-19 16:27:58 +0200

    Solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -12,23 +12,18 @@ function applyCustomIndexOf() {
 
     if (fromIndex >= 0) {
       for (let i = fromIndex; i < this.length; i++) {
-        if (typeof (this[i]) !== typeof (searchElement)) {
-          return -1;
-        }
         if (this[i] === searchElement) {
           return i;
         }
       }
     } else {
       for (let i = this.length + fromIndex; i < this.length; i++) {
-        if (typeof (this[i]) !== typeof (searchElement)) {
-          return -1;
-        }
         if (this[i] === searchElement) {
           return i;
         }
       }
     }
+    return -1;
   };
 }
 
