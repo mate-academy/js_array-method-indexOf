9cb9137694f222397ddab2ecd9ee63e30f944224
natya2203 <nastya_2203@ukr.net>
2019-11-19 16:05:10 +0200

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,6 +6,15 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    if (fromIndex < 0) {
+      // eslint-disable-next-line no-cond-assign,no-param-reassign
+      fromIndex = fromIndex + [].length;
+      if (fromIndex < 0) {
+        // eslint-disable-next-line no-param-reassign
+        fromIndex = 0;
+      }
+    }
+
     for (let i = fromIndex; i < this.length; i++) {
       if (this[i] === searchElement && fromIndex >= 0) {
         return i;
