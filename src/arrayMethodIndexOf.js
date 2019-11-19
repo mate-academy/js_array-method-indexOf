59db9f45047cefd9fb78da6f917aa0433c6b4ccc
Denys <denys.cheporniuk@gmail.com>
2019-11-19 15:31:34 +0200

    fix

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,12 +6,19 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
-    for (let i = fromIndex; i < this.length; i++) {
-      if (this[i] === searchElement) {
-        return i;
+    if (fromIndex < 0) {
+      for (let i = this.length + fromIndex; i < this.length; i++) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+    } else {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (this[i] === searchElement) {
+          return i;
+        }
       }
     }
-
     return -1;
   };
 }
