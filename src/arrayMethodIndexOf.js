1f90f40558239ee074fd2e08e1dafde09313528e
Liliya Taran <liliyataran2017@gmail.com>
2019-11-19 16:08:03 +0200

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,8 +5,30 @@
  *
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    if (searchElement === undefined) {
+      return -1;
+    }
+
+    if (fromIndex >= 0) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (Object.is(searchElement, this[i])
+          || (isNaN(searchElement)
+            && isNaN(searchElement) === isNaN(this[i]))) {
+          return i;
+        }
+      }
+    } else {
+      for (let i = this.length + fromIndex; i < this.length; i++) {
+        if (Object.is(searchElement, this[i])
+          || (isNaN(searchElement)
+            && isNaN(searchElement) === isNaN(this[i]))) {
+          return i;
+        }
+      }
+    }
+
+    return -1;
   };
 }
 
