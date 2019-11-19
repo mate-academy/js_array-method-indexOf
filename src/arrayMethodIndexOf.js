a7b0971add7b85a1d51073f8c2a7bcf5d488df98
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-11-19 23:23:44 +0200

    Aidinov Alexii

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -4,8 +4,34 @@
  * Implement method indexOf
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    if (this.length === 0 || arguments.length === 0) {
+      return -1;
+    }
+
+    let start;
+
+    if (fromIndex < 0) {
+      start = this.length - 1 + fromIndex;
+    } else {
+      start = fromIndex;
+    }
+
+    if (fromIndex >= this.length) {
+      return -1;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+
+      if (isNaN(this[i]) && isNaN(searchElement) && this[i] !== undefined) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
