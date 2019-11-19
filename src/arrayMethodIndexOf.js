b37b213a161872e421130c49ffe9a392ac497311
mgrinko <glukym@gmail.com>
2019-11-19 22:24:56 +0200

    descriptions was updated

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -1,7 +1,9 @@
 'use strict';
 
 /**
- * Implement method indexOf
+ * Implement improved version of indexOf method. It should correctly search for
+ * NaN and undefined and return -1 if searchElement is not given. It should
+ * also work for negative fromIndex handling -2 as a pre last element.
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
