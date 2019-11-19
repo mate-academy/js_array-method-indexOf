b1f9a500c616559b0135255881033b72cd517d78
helmax-y <max_rad@ukr.net>
2019-11-19 19:53:37 +0200

    Solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,7 +5,25 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    let i = 0;
+    if (isFinite(fromIndex) && fromIndex >= 0) {
+      i = fromIndex;
+    } else if (isFinite(fromIndex) && fromIndex < 0) {
+      i = this.length + fromIndex;
+    }
+
+    for (i; i < this.length; i++) {
+      if ((!isNaN(searchElement) && searchElement === this[i])
+        || (isNaN(searchElement) && Object.is(searchElement, this[i]))) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
