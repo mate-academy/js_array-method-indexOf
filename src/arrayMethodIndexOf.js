8cd0f4a3ba48fe4364daa3ce1b93fe7e7f8c9b17
Stanislav Mochyr <delenjer@gmail.com>
2019-11-20 16:35:36 +0200

    Solutio

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -4,8 +4,26 @@
  * Implement method indexOf
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    let startArg = fromIndex;
+    if (startArg < 0) {
+      startArg += this.length;
+    }
+    if (startArg < 0) {
+      startArg = 0;
+    }
+    for (let i = startArg; i < this.length; i++) {
+      // console.log(this.length);
+      if (Object.is(this[i], searchElement)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
