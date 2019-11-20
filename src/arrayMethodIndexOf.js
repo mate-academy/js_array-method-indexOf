5ffe4798c1dfb9920594370409bc3bf7f96d58f3
Vlad Mishchenko <vladislav.a.mishchenko@gmail.com>
2019-11-20 11:37:02 +0200

    develop

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,16 +6,25 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
-    let i = fromIndex;
-    if (i < 0) {
+    if (arguments.length === 0) {
       return -1;
     }
+
+    let i = fromIndex;
+
+    if (i < 0) {
+      i += this.length;
+    }
+
+    if (i > this.length) {
+      return -1;
+    }
+
     for (; i < this.length; i++) {
-      if (this[i] === searchElement) {
+      if (Object.is(this[i], searchElement)) {
         return i;
       }
     }
-
     return -1;
   };
 }
