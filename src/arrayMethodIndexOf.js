ad609585f796c985026c65d2ddd0c99801f5a120
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-24 23:24:06 +0200

    done

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -7,7 +7,44 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    if (this.length === 0) {
+      return -1;
+    }
+
+    if (arguments.length < 1) {
+      return -1;
+    }
+
+    let findValue = searchElement;
+    let indexFrom = fromIndex || 0;
+
+    if (searchElement === null) {
+      findValue = null;
+    }
+
+    if (searchElement === undefined) {
+      findValue = undefined;
+    }
+
+    if (Math.sign(indexFrom) < 0) {
+      indexFrom = this.length - Math.abs(indexFrom);
+
+      if (Math.sign(indexFrom) < 0) {
+        indexFrom = 0;
+      }
+    }
+
+    for (let i = indexFrom; i < this.length; i++) {
+      if (Number.isNaN(this[i]) && Number.isNaN(findValue)) {
+        return i;
+      }
+
+      if (this[i] === findValue) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
