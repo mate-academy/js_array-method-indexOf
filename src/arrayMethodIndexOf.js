535cb1bce5134be12e664e46076e360b85de4133
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-01-26 20:52:06 +0200

    add solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -7,7 +7,63 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    if (searchElement === undefined) {
+      for (let i = 0; i < this.length; i++) {
+        if (this[i] === undefined) {
+          return i;
+        }
+      }
+    }
+
+    if (Math.abs(fromIndex) > this.length && fromIndex < 0) {
+      for (let i = 0; i < this.length; i++) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+    }
+
+    if (isNaN(searchElement)) {
+      for (let i = 0; i < this.length; i++) {
+        if (Number.isNaN(this[i])) {
+          return i;
+        }
+      }
+    }
+
+    if (fromIndex < 0) {
+      const negIndex = this.length + fromIndex;
+
+      for (let i = this.length - 1; i > negIndex - 1; i--) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+
+      return -1;
+    }
+
+    if (fromIndex !== undefined) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+
+      return -1;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
