9a70ac172e9b3ab8c85ca9a0ec7bda6205c2c4da
Natalia Lypovyk <cardinnata@gmail.com>
2020-01-26 16:06:49 +0200

    IndexOf implementation

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -7,7 +7,31 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let index = fromIndex || 0;
+
+    if (arguments.length < 1 || fromIndex > this.length) {
+      return -1;
+    }
+
+    if (fromIndex <= -this.length) {
+      index = 0;
+    }
+
+    if (fromIndex < 0 && Math.abs(fromIndex) < this.length) {
+      index = this.length + fromIndex;
+    }
+
+    for (index; index < this.length; index++) {
+      if (Number.isNaN(this[index]) && Number.isNaN(searchElement)) {
+        return index;
+      }
+
+      if (this[index] === searchElement) {
+        return index;
+      }
+    }
+
+    return -1;
   };
 }
 
