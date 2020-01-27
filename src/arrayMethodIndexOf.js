7336de1af3af9c830bfbfaef69deed91d62fc10f
Senya Volkov <formobileprima@gmail.com>
2020-01-27 12:41:10 +0200

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -7,8 +7,31 @@
  */
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let index;
+    const mainArr = this;
+    const emptyArg = [...arguments];
+
+    if (emptyArg.length === 0) {
+      return -1;
+    }
+
+    if (fromIndex === Math.abs(fromIndex)) {
+      index = fromIndex || 0;
+    } else {
+      index = Math.abs(fromIndex) < mainArr.length ? mainArr.length
+        - Math.abs(fromIndex) : 0;
+    }
+
+    for (let i = index; i < mainArr.length; i++) {
+      if (Object.is(mainArr[i], searchElement)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
+
+applyCustomIndexOf();
 
 module.exports = applyCustomIndexOf;
