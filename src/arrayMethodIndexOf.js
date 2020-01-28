8d482ad77cbb214923e8bc73dad6365e9c1a4233
Mikhail <mikhail.sapuzhak@gmail.com>
2020-01-28 13:10:45 +0200

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -5,9 +5,31 @@
  * NaN and undefined and return -1 if searchElement is not given. It should
  * also work for negative fromIndex handling -2 as a pre last element.
  */
+
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    if (!arguments.length || fromIndex >= this.length) {
+      return -1;
+    }
+
+    let searchFrom = fromIndex;
+
+    if (fromIndex < 0) {
+      searchFrom = this.length + fromIndex;
+    }
+
+    if (searchFrom < 0) {
+      searchFrom = 0;
+    }
+
+    for (let i = searchFrom; i < this.length; i++) {
+      if (searchElement === this[i]
+        || (Number.isNaN(searchElement) && Number.isNaN(this[i]))) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
