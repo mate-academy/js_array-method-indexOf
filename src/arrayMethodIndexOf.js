7841f8c1917edb55eb62a4423c78deb1e5c02a8f
ronyshchenko <roma.onyshchenko@gmail.com>
2020-02-07 17:19:50 +0200

    7_2_20_first

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,8 +6,52 @@
  * also work for negative fromIndex handling -2 as a pre last element.
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(valueToFind, from = 0) {
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    let fromIndex = from;
+
+    if (fromIndex >= this.length) {
+      return -1;
+    }
+
+    if (fromIndex <= (-1) * this.length) {
+      fromIndex = 0;
+    }
+
+    if (fromIndex >= 0) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (((typeof valueToFind === 'number'
+        && isNaN(valueToFind))
+        && (typeof this[i] === 'number' && isNaN(this[i])))
+        || (this[i] === valueToFind)) {
+          return i;
+        }
+
+        if (this[i] === valueToFind) {
+          return i;
+        }
+      }
+
+      return -1;
+    }
+
+    if (fromIndex < 0) {
+      fromIndex = this.length + fromIndex;
+
+      for (let i = fromIndex; i < this.length; i++) {
+        if (((typeof valueToFind === 'number'
+        && isNaN(valueToFind))
+        && (typeof this[i] === 'number' && isNaN(this[i])))
+        || (this[i] === valueToFind)) {
+          return i;
+        }
+      }
+
+      return -1;
+    }
   };
 }
 
