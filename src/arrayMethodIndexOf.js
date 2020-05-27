87d0772f06bb65d2c7c3112692016b61200070bc
Yura <wa7er14@gmail.com>
2020-05-27 17:47:07 +0300

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,8 +6,24 @@
  * also work for negative fromIndex handling -2 as a pre last element.
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    let curIndex = fromIndex >= 0 ? fromIndex : this.length + fromIndex;
+
+    if (Math.abs(curIndex) > this.length) {
+      if (curIndex > this.length) {
+        return -1;
+      }
+      curIndex = 0;
+    }
+
+    for (curIndex; curIndex < this.length; curIndex++) {
+      if (this[curIndex] === searchElement
+        || (Number.isNaN(this[curIndex]) && Number.isNaN(searchElement))) {
+        return curIndex;
+      }
+    }
+
+    return -1;
   };
 }
 
