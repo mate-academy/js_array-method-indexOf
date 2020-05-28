5bfe8b5f8d40e40395da9dfa73e5313c3a346ac0
DTR <t.terry.rice@gmail.com>
2020-05-28 21:06:17 +0300

    final 1.0

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -1,5 +1,5 @@
 'use strict';
-/* eslint-disable */
+
 /**
  * Implement improved version of indexOf method. It should correctly search for
  * NaN and undefined and return -1 if searchElement is not given. It should
@@ -8,47 +8,29 @@
 
 function applyCustomIndexOf() {
   [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    const founded = {};
+    let searcher = fromIndex;
 
-    let founded = [];
-    (fromIndex < 0) ? (fromIndex = this.length + fromIndex) : fromIndex;
-    if(fromIndex < 0) {fromIndex = 0};
+    if (searcher < 0) {
+      searcher = this.length + searcher;
+    }
 
-    for(let i = fromIndex; i <= this.length; i++) {
-      if(Object.is(searchElement, this[i])) {founded[0] = i; break};
-    }    
+    if (searcher <= -1) {
+      searcher = 0;
+    };
 
+    for (let i = searcher; i <= this.length; i++) {
+      if (Object.is(searchElement, this[i])) {
+        founded.answer = i; break;
+      }
+    };
 
-    if (founded[0] === undefined) {founded[0] = -1}
-    return founded[0];
+    if (founded.answer === undefined) {
+      founded.answer = -1;
+    };
 
+    return founded.answer;
   };
 }
 
 module.exports = applyCustomIndexOf;
-
-
-
-/*
-
-function iF (thisIs, searchElement, fromIndex = 0) {
-    let founded = [];
-    (fromIndex < 0) ? (fromIndex = thisIs.length + fromIndex) : fromIndex;
-    if(fromIndex < 0) {fromIndex = 0};
-    console.log("iF -> thisIs.length", thisIs.length)
-    console.log("iF -> fromIndex", fromIndex)
-
-    for(let i = fromIndex; i <= thisIs.length; i++) {
-      if(searchElement === thisIs[i]) {founded = i; break};
-    }
-
-    console.log("iF -> founded", founded)
-    if (founded === []) {founded[0] = -1}
-    return founded;
-  }
-
-const  a = [ '11', undefined, NaN, null, '11', 44 ];
-console.log("if", iF(a, undefined, -999))
-
-console.log("i", a.indexOf(undefined, -999));
-
-// */
