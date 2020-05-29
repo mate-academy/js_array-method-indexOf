18b12fa3e4f710de651046066b0ed2c44a68b488
Slava Polishchuk <dizerllc@gmail.com>
2020-05-29 19:33:08 +0300

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,9 +6,57 @@
  * also work for negative fromIndex handling -2 as a pre last element.
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    if (arguments.length === 0) {
+      return 1;
+    }
+
+    if (!this.length) {
+      return -1;
+    }
+
+    if (fromIndex >= 0) {
+      for (let i = fromIndex; i < this.length; i++) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+    }
+
+    if (fromIndex < 0 && fromIndex > (-this.length)) {
+      for (let i = (this.length + fromIndex); i < this.length; i++) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+    }
+
+    if (Number.isNaN(searchElement)) {
+      for (let i = 0; i < this.length; i++) {
+        if (Number.isNaN(this[i]) === Number.isNaN(searchElement)) {
+          return i;
+        }
+      }
+    }
+
+    if (fromIndex < (-this.length)) {
+      if (fromIndex < (-this.length)) {
+        for (let i = 0; i < this.length; i++) {
+          if (this[i] === searchElement) {
+            return i;
+          }
+        }
+      }
+    }
+
+    return -1;
   };
 }
 
 module.exports = applyCustomIndexOf;
+
+// applyCustomIndexOf();
+
+// const source = ['11', undefined, NaN, null, '11', 44];
+
+// console.log(source.indexOf2(undefined, -5));
