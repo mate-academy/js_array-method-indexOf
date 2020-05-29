7e4d062f0d2299b438e929e31ea8e15239cad27a
DTR <t.terry.rice@gmail.com>
2020-05-29 15:41:39 +0300

    final 1.1

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/**
+/*
  * Implement improved version of indexOf method. It should correctly search for
  * NaN and undefined and return -1 if searchElement is not given. It should
  * also work for negative fromIndex handling -2 as a pre last element.
