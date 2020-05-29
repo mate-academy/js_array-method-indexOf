47015a87b8e67303e1cd4c294ba557877bb5df2d
LimonovInsp <bis.limonov@gmail.com>
2020-05-29 15:55:44 +0300

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -6,8 +6,23 @@
  * also work for negative fromIndex handling -2 as a pre last element.
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    let startIndex = fromIndex < 0
+      ? (this.length + fromIndex) : fromIndex;
+
+    if (fromIndex > this.length) {
+      return -1;
+    } else if (fromIndex < -this.length) {
+      startIndex = 0;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      if (Object.is(this[i], searchElement)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
