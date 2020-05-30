1d14129f4276516fc7aeaf5156560f267d3d7a00
Andrew Nickyforov <nik_andrey@ukr.net>
2020-05-30 16:29:36 +0300

    solution

--- src/arrayMethodIndexOf.js
+++ src/arrayMethodIndexOf.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unreachable */
 'use strict';
 
 /**
@@ -6,8 +7,26 @@
  * also work for negative fromIndex handling -2 as a pre last element.
  */
 function applyCustomIndexOf() {
-  [].__proto__.indexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.indexOf2 = function(searchElement, fromIndex = 0) {
+    let startIndex = fromIndex;
+
+    if (fromIndex < (-this.length)) {
+      startIndex = 0;
+    } else if (fromIndex < 0 && fromIndex >= (-this.length)) {
+      startIndex = this.length + fromIndex;
+    }
+
+    if (this.length === 0) {
+      return -1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      if (Object.is(this[i], searchElement)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
